70502c9c9b90a42ed181ac325425a5da87743af3
snoop1st <sokolis.nikolas@gmail.com>
2023-12-20 18:42:25 +0200

    Fix small login issue

--- public/js/login.js
+++ public/js/login.js
@@ -92,40 +92,26 @@ function getSessionCookie(username) {
 function login() {
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
-    const userInfo = document.getElementById('user-info');
-    const loggedUsername = document.getElementById('logged-username');
-  
-    const username = usernameInput.value.trim(); // Trim whitespace
-    const password = passwordInput.value.trim(); // Trim whitespace
-  
+
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
+
     const authenticationResult = authenticateUser(username, password);
-  
+
     if (authenticationResult.statusCode === 200) {
-      // Close the login popup upon successful login
-      closeLoginPopup();
-  
-      // Set the jsonResponse as a global variable
-      window.jsonResponse = {
-        sessionId: authenticationResult.body.sessionId,
-        username: username,
-        uuid: authenticationResult.body.sessionId,
-      };
-  
-      // Save user session data in a cookie
-      setSessionCookie(username, window.jsonResponse);
-  
-      // Update the user-info section
-      loggedUsername.textContent = username;
-      userInfo.style.display = 'block';
-  
-      console.log('Logged in with UUID:', window.jsonResponse.uuid);
-      console.log('Logged in with Username:', window.jsonResponse.username);
-  
-      // Optionally update UI to show user's favorites after login
-      updateFavoritesUI(window.jsonResponse.uuid);
+        closeLoginPopup();
+        window.jsonResponse = {
+            sessionId: authenticationResult.body.sessionId,
+            username: username,
+            uuid: authenticationResult.body.sessionId,
+        };
+        localStorage.setItem('jsonResponse', JSON.stringify(window.jsonResponse));
+        updateUserInfo(window.jsonResponse);
+
+        console.log('Logged in with UUID:', window.jsonResponse.uuid);
+        console.log('Logged in with Username:', window.jsonResponse.username);
     } else {
-      // Display an error message for unsuccessful authentication
-      console.error('Error:', authenticationResult.body.error);
-      alert('Invalid credentials. Please try again.');
+        console.error('Error:', authenticationResult.body.error);
+        alert('Invalid credentials. Please try again.');
     }
 }
